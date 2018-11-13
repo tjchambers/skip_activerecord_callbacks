8dc3cee801a500d86835e7a08f6323e900a19768
Tim Chambers <tim@possibilogy.com>
2018-11-13 10:20:55 -0800

    Boost to 44.0 of Rails for test

--- skip_activerecord_callbacks.gemspec
+++ skip_activerecord_callbacks.gemspec
@@ -16,9 +16,10 @@ Gem::Specification.new do |s|
   s.files = Dir["{lib}/**/*"] + ["MIT-LICENSE", "Rakefile", "README.md", "CHANGELOG.md"]
   s.test_files = Dir["test/**/*"]
 
-  s.add_dependency "rails", "~> 3.2"
+  s.add_dependency "rails", "~> 4.0"
 
   s.add_development_dependency "sqlite3"
   s.add_development_dependency "minitest"
   s.add_development_dependency "mocha"
+  s.add_development_dependency "test-unit"
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,19 +1,18 @@
 # Configure Rails Environment
 ENV["RAILS_ENV"] = "test"
 
-require File.expand_path("../dummy/config/environment.rb",  __FILE__)
+require File.expand_path("../dummy/config/environment.rb", __FILE__)
 require "rails/test_help"
 require 'minitest/autorun'
 require 'mocha/integration/mini_test'
+require 'mocha/test_unit'
 
 Rails.backtrace_cleaner.remove_silencers!
 
 ActiveRecord::Base.establish_connection(:adapter => 'sqlite3', :database => ':memory:')
 ActiveRecord::Base.class_eval do
-  silence do
-    connection.create_table :products, :force => true do |t|
-      t.column :name, :string
-    end
+  connection.create_table :products, :force => true do |t|
+    t.column :name, :string
   end
 end
 
